9fde1800246be1ce979ab94cc10516205025f657
Raghav-0311 <raghavkashyap0311@gmail.com>
2023-12-15 16:22:15 +0530

    Tested eShop API, It's Working Fine ✅🚀

--- src/context/productContext.jsx
+++ src/context/productContext.jsx
@@ -3,13 +3,30 @@
 // 2. provider 
 // 3. consumer - to make it simple we use useContext Hook
 
-import { createContext, useContext } from "react";
+import axios from "axios";
+import { createContext, useContext, useEffect } from "react";
 
 // create context
 const AppContext = createContext();
 
+const TOP_GADGETS = "/top_e_gadgets.json"; // Endpoint
+
+const API = `https://raghav-0311.github.io/eShop-API${TOP_GADGETS}`;
+
 // provider
 const AppProvider = ({ children }) => {
+
+    const getProducts = async (url) => {
+        const res = await axios.get(url);
+        // console.log(res.data.data); // TESTED ✅
+        const allProducts = await res.data.data;
+        // console.log(allProducts); // TESTED ✅
+    }
+
+    useEffect(() => {
+        getProducts(API);
+    }, []);
+
     return <AppContext.Provider value="Raghav">
         {children}
     </AppContext.Provider>
