a44143b57f1e1453d8754fc09b06cfb4ac115097
Raghav-0311 <raghavkashyap0311@gmail.com>
2023-12-16 11:46:32 +0530

    Formated Price as per Intl Currency Format ₹✅

---
+++ src/helpers/FormatPrice.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormatPrice = ({ price }) => {
+  return Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "INR",
+    maximumFractionDigits: 0,
+  }).format(price / 1);
+};
+
+export default FormatPrice
