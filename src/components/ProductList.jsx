7de43f8c56933e9b8fb2c80cd5d511692b89c9f5
Raghav-0311 <raghavkashyap0311@gmail.com>
2023-12-23 02:46:25 +0530

    Products Page Initial Setup ✅

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductList = () => {
+  return (
+    <div>
+      Product List Section
+    </div>
+  )
+}
+
+export default ProductList
---
+++ src/components/Sort.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sort = () => {
+  return (
+    <div>
+      Sort Section
+    </div>
+  )
+}
+
+export default Sort
