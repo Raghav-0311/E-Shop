12b3c7603c3eaad9234b6ae1ed683dd4f572e49a
Raghav-0311 <raghavkashyap0311@gmail.com>
2023-12-23 03:35:59 +0530

    Filter Context & Filter Reduces setup for Products ✅

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { AppProvider } from './context/productContext.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { AppProvider } from "./context/productContext.jsx";
+import { FilterContextProvider } from "./context/filterContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <AppProvider>
-    <App />
-  </AppProvider>,
-)
+    <FilterContextProvider>
+      <App />
+    </FilterContextProvider>
+  </AppProvider>
+);
