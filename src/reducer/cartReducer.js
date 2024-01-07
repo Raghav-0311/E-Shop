ff3536ce991a32f2d0dafc51f13aec6837ae76be
Raghav-0311 <raghavkashyap0311@gmail.com>
2024-01-07 06:13:35 +0530

    Handled Existing Items in Cart ⚙️ ✅

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -4,21 +4,49 @@ const cartReducer = (state, action) => {
         let { _id, color, quantity, singleProduct } = action.payload;
         // console.log(singleProduct); // TESTED ✅
 
-        let cartProduct;
+        // Tackle The Existing Product 
+        let existingProduct = state.cart.find((curItem) => curItem._id === _id + color);
+        // console.log(existingProduct); // TESTED ✅
 
-        cartProduct = {
-            _id: _id + color,
-            name: singleProduct.name,
-            color,
-            quantity,
-            image: singleProduct.image,
-            price: singleProduct.price,
-            max: 5 // Product Stock - can't add more than 5
-        };
+        if (existingProduct) {
+            let updatedProduct = state.cart.map((curElem) => {
+                if (curElem._id === _id + color) {
+                    let newQuantity = curElem.quantity + quantity;
 
-        return {
-            ...state,
-            cart: [...state.cart, cartProduct],
+                    if (newQuantity >= curElem.max) {
+                        newQuantity = curElem.max;
+                    }
+
+                    return {
+                        ...curElem,
+                        quantity: newQuantity,
+                    }
+                } else {
+                    return curElem;
+                }
+            });
+
+            return {
+                ...state,
+                cart: updatedProduct,
+            }
+        } else {
+
+            
+            let cartProduct = {
+                _id: _id + color,
+                name: singleProduct.name,
+                color,
+                quantity,
+                image: singleProduct.image,
+                price: singleProduct.price,
+                max: 5 // Product Stock - can't add more than 5
+            };
+            
+            return {
+                ...state,
+                cart: [...state.cart, cartProduct],
+            }
         }
     }
 
