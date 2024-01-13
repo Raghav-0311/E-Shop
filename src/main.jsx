310ac578c17776bfc864191e3f535555b189e587
Raghav-0311 <raghavkashyap0311@gmail.com>
2024-01-13 08:13:48 +0530

    Configured Auth0 Provider for eShop Login / Signup ⚙️

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { Auth0Provider } from '@auth0/auth0-react';
 import { AppProvider } from "./context/productContext.jsx";
 import { FilterContextProvider } from "./context/filterContext.jsx";
 import { CartProvider } from "./context/cartContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <AppProvider>
-    <FilterContextProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterContextProvider>
-  </AppProvider>
+  <Auth0Provider
+    domain="dev-zoj6c1mip8xqabpq.us.auth0.com"
+    clientId="NhIr5E7jWGggwDDJScNKFaIdQcVj45JQ"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <AppProvider>
+      <FilterContextProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterContextProvider>
+    </AppProvider>
+  </Auth0Provider>
 );
